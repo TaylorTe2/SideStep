5b378d1f328fe8f6b0ff9f9b9f3d4eb620715340
TaylorTe2 <85718413+TaylorTe2@users.noreply.github.com>
2022-04-06 18:43:50 +1000

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/sidestep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sidestep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:collection';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.8688, 151.2093);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SideStep'),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          myLocationButtonEnabled: true,
+        ),
+      ),
+    );
+  }
+}
