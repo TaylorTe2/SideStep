847f3e548b81dd79a6d065087bc5d7216360eb6e
s3863084 <s3863084@student.rmit.edu.au>
2022-05-10 22:45:54 +1000

    Working Map Direction
    New Base App with working Directions API.
    
    Uses a new API key needs replacement

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,184 @@
-<<<<<<< HEAD
 import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-=======
-import 'dart:ui';
-import 'UserSettings.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sidestep/location_service.dart';
+import 'package:sidestep/UserSettings.dart';
 
-import 'dart:math' show cos, sqrt, asin;
->>>>>>> 381e2abaa3dd19be69b1c94f9699b8612bde82e1
+void main() => runApp(MyApp());
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
 
-//default starting position on map when the app is run. We should change this to device location in the future.
-  final LatLng _center = const LatLng(-27.4705, 153.0260);
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  //places Markers in list I don't actually know why this has to be an array but it makes it work.
-  List<Marker> myMarker = [];
+  //Used to take input
+  TextEditingController _originController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
 
-// Creates a Google map controller called controller and uses it as the map controller when the map is created.
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  Set<Polyline> _polyline = Set<Polyline>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+
+  int _polygonIdCounter = 1;
+  int _polylineIdCounter = 1;
+
+  static final CameraPosition _mainLocation =
+      CameraPosition(target: LatLng(-27.4705, 153.0260), zoom: 14.4746);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setMarker(LatLng(-27.4705, 153.0260));
   }
 
-// function that will add a marker at the position of the tappedPoint to myMarker[]
-  _handleTap(LatLng tappedPoint) async {
+  void _setMarker(LatLng point) {
     setState(() {
-      myMarker = [];
-      myMarker.add(
-        Marker(
-          markerId: MarkerId(tappedPoint.toString()),
-          position: tappedPoint,
-        ),
+      _markers.add(
+        Marker(markerId: MarkerId('marker'), position: point),
       );
     });
   }
 
+  void _setPolyline(List<PointLatLng> points) {
+    final String polylineIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polyline.add(Polyline(
+      polylineId: PolylineId(polylineIdVal),
+      width: 5,
+      color: Colors.orange,
+      points: points
+          .map(
+            (point) => LatLng(point.latitude, point.longitude),
+          )
+          .toList(),
+    ));
+  }
+
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polygons.add(Polygon(
+        polygonId: PolygonId(polygonIdVal),
+        points: polygonLatLngs,
+        strokeWidth: 2,
+        fillColor: Colors.transparent));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Stack(
-        alignment: AlignmentDirectional.topCenter,
+    return Scaffold(
+      appBar: AppBar(title: Text("SideStep")),
+      body: Column(
         children: [
-          Container(
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _originController,
+                      decoration: InputDecoration(hintText: 'Origin'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    TextFormField(
+                      controller: _destinationController,
+                      decoration: InputDecoration(hintText: 'Where to?'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: FloatingActionButton.extended(
+                        label: Text('Next'),
+                        backgroundColor: Color(0xFFF15A24),
+                        icon: Icon(
+                          Icons.arrow_circle_right,
+                          size: 20.0,
+                        ),
+                        onPressed: () async {
+                          var directions = await LocationService()
+                              .getDirections(_originController.text,
+                                  _destinationController.text);
+
+                          _goToPlace(
+                            directions['start_location']['lat'],
+                            directions['start_location']['lng'],
+                            directions['bounds_ne'],
+                            directions['bounds_sw'],
+                          );
+                          _setPolyline(directions['polyline_decoded']);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
             child: GoogleMap(
-              markers: Set.from(myMarker),
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-              myLocationEnabled: true,
-              myLocationButtonEnabled: false,
-              compassEnabled: true,
-              onTap: _handleTap,
-              zoomControlsEnabled: false,
-              zoomGesturesEnabled: true,
+              mapType: MapType.normal,
+              markers: _markers,
+              polygons: _polygons,
+              polylines: _polyline,
+              initialCameraPosition: _mainLocation,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50.0, left: 10, right: 10),
-            child: Material(
-              child: Container(
-                height: 30,
-                child: TextField(
-                  textAlignVertical: TextAlignVertical(y: 1.0),
-                  autocorrect: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Search Location',
-                    hintTextDirection: TextDirection.ltr,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Builder(builder: (context) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(25.0, 0, 0, 50.0),
-              child: Container(
-                alignment: Alignment.bottomLeft,
-                child: FloatingActionButton(
-                  backgroundColor: Colors.orange,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Settings()),
-                    );
-                  },
-                  child: const Icon(
-                    Icons.menu,
-                    size: 40,
-                  ),
-                ),
-              ),
-            );
-          }),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Settings()),
+          );
+        },
+        //label: Text(''),
+        backgroundColor: Color(0xFFF15A24),
+        child: const Icon(Icons.settings),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
+  }
+
+  Future<void> _goToPlace(double lat, double lng, Map<String, dynamic> boundsNe,
+      Map<String, dynamic> boundsSw) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: LatLng(lat, lng), zoom: 12),
+    ));
+
+    controller.animateCamera(CameraUpdate.newLatLngBounds(
+        LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng'])),
+        25));
+
+    _setMarker(LatLng(lat, lng));
   }
 }
