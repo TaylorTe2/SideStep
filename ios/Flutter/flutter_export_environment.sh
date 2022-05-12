03581548b2d4145ed42458a69efa5b126d03ced8
Taylor Neal <s3873735@studen.rmit.edu.au>
2022-05-12 20:53:21 +1000

    made gitignore kinda work

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\USER\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\USER\Desktop\Uni\Building IT Systems\Assessment 2\SideStep-1"
+export "FLUTTER_ROOT=C:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Taylor\Uni\RMIT\Building_IT_Systems\Project\SideStep"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
