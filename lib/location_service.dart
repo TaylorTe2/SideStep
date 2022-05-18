d105ef9b9c77e821a0bc8e4a340a93bce8ff9251
s3863084 <s3863084@student.rmit.edu.au>
2022-05-18 17:03:19 +1000

    3 Working Polyline and Map Styles
    New version containing 3 polylines and map style.

--- lib/location_service.dart
+++ lib/location_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
@@ -33,22 +34,68 @@ class LocationService {
 
   Future<Map<String, dynamic>> getDirections(
       String orgin, String destination) async {
-    final String url =
+    String fastest =
         'https://maps.googleapis.com/maps/api/directions/json?origin=$orgin&destination=$destination&key=$key';
+    String noToll =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$orgin&destination=$destination&avoid=tolls&key=$key';
 
-    var response = await http.get(Uri.parse(url));
-    var json = convert.jsonDecode(response.body);
+    var responseFastest = await http.get(Uri.parse(fastest));
+    var responsenoToll = await http.get(Uri.parse(noToll));
+    var jsonFastest = convert.jsonDecode(responseFastest.body);
+    var jsonNoToll = convert.jsonDecode(responsenoToll.body);
+
+    // Calculate Flexible
+
+    //Halfway point
+    var _midPoint = jsonNoToll['routes'][0]['legs'][0]['distance']['value'] / 2;
+
+    double _flexiLat = 0;
+    double _flexiLng = 0;
+
+    //Find which step crosses halfway point
+    for (var i = 0; 1 <= _midPoint; i++) {
+      _midPoint = _midPoint -
+          jsonNoToll['routes'][0]['legs'][0]['steps'][i]['distance']['value'];
+      if (_midPoint <= 0) {
+        print("Found MidPOINT");
+        _flexiLat = jsonNoToll['routes'][0]['legs'][0]['steps'][i]
+            ['end_location']['lat'];
+        _flexiLng = jsonNoToll['routes'][0]['legs'][0]['steps'][i]
+            ['end_location']['lng'];
+      }
+    }
+
+    print("Build URLS");
+    String flexi1 =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$orgin&destination=$_flexiLat,$_flexiLng&avoid=tolls&key=$key';
+    String flexi2 =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$_flexiLat,$_flexiLng&destination=$destination&key=$key';
+
+    print(flexi2);
+
+    var responseflexi1 = await http.get(Uri.parse(flexi1));
+    var responseflexi2 = await http.get(Uri.parse(flexi2));
+    var jsonFlexi1 = convert.jsonDecode(responseflexi1.body);
+    var jsonFlexi2 = convert.jsonDecode(responseflexi2.body);
+
+    print("JSON GOOD!");
+
     var results = {
-      'bounds_ne': json['routes'][0]['bounds']['northeast'],
-      'bounds_sw': json['routes'][0]['bounds']['southwest'],
-      'start_location': json['routes'][0]['legs'][0]['start_location'],
-      'end_location': json['routes'][0]['legs'][0]['end_location'],
-      'polyline': json['routes'][0]['overview_polyline']['points'],
-      'polyline_decoded': PolylinePoints()
-          .decodePolyline(json['routes'][0]['overview_polyline']['points'])
+      'bounds_ne': jsonFastest['routes'][0]['bounds']['northeast'],
+      'bounds_sw': jsonFastest['routes'][0]['bounds']['southwest'],
+      'start_location': jsonFastest['routes'][0]['legs'][0]['start_location'],
+      'end_location': jsonFastest['routes'][0]['legs'][0]['end_location'],
+      'polyline': jsonFastest['routes'][0]['overview_polyline']['points'],
+      'polyline_decoded_fastest': PolylinePoints().decodePolyline(
+          jsonFastest['routes'][0]['overview_polyline']['points']),
+      'polyline_decoded_noToll': PolylinePoints().decodePolyline(
+          jsonNoToll['routes'][0]['overview_polyline']['points']),
+      'polyline_decoded_flexi1': PolylinePoints().decodePolyline(
+          jsonFlexi1['routes'][0]['overview_polyline']['points']),
+      'polyline_decoded_flexi2': PolylinePoints().decodePolyline(
+          jsonFlexi2['routes'][0]['overview_polyline']['points']),
     };
-
-    //print(results);
+    print("results sent!");
     return results;
   }
 }
